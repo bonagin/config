00e7c96174d5bd299ea10f84625df66ceca44553
bonagin <b.v.mtimkulu@gmail.com>
2025-03-16 15:43:47 +0200

    Updates to ignore the file errors if no file is parsed

--- config.go
+++ config.go
@@ -12,16 +12,20 @@ type NullValue struct {
 	NotNull bool
 }
 
-var Config map[string]NullValue
-var ConfigFileName NullValue
+var (
+	Config           map[string]NullValue
+	ConfigFileName   NullValue
+	ConfigFileParsed bool
+)
 
 /*
-	Initialize a new JSON config instance
+Initialize a new JSON config instance
 */
 func NewConfig() {
 	args := os.Args
 	print := false
 	filename := ""
+	ConfigFileParsed = false
 
 	for i, arg := range args {
 		if arg == "--config" {
@@ -29,49 +33,56 @@ func NewConfig() {
 				filename = args[i+1]
 			}
 		}
-	}
-
-	if filename == "" {
-		log.Fatal("\nConfig tag not set\n\t '" + os.Args[0] + "' --config /path/to/json/configfile'")
-	}
-
-	file, err := os.Open(filename)
-	if err != nil {
-		log.Fatal("Failed to open config file ", err.Error())
-	}
-
-	config, err := ioutil.ReadAll(file)
-	if err != nil {
-		log.Fatal("Failed to read config file : ", err.Error())
-	}
-
-	var objmap map[string]*json.RawMessage
-	err = json.Unmarshal(config, &objmap)
-	if err != nil {
-		log.Fatal("Failed to process config file : ", err.Error())
-	}
-
-	if Config == nil {
-		Config = make(map[string]NullValue)
-	}
-
-	for key, value := range objmap {
-		val := string(*value)
-
-		if len(val) > 0 && val[0] == '"' {
-			val = val[1:]
-		}
-		if len(val) > 0 && val[len(val)-1] == '"' {
-			val = val[:len(val)-1]
-		}
-
-		Config[key] = NullValue{Value: val, NotNull: true}
-		if print {
-			log.Println(key, "=", val)
+		if arg == "--print_config" {
+			if len(args) > (i + 1) {
+				print = true
+			}
 		}
 	}
 
-	ConfigFileName = NullValue{Value: file.Name(), NotNull: true}
+	if filename != "" {
+		file, err := os.Open(filename)
+		if err != nil {
+			log.Fatal("Failed to open config file ", err.Error())
+		}
+
+		config, err := ioutil.ReadAll(file)
+		if err != nil {
+			log.Fatal("Failed to read config file : ", err.Error())
+		}
+
+		var objmap map[string]*json.RawMessage
+		err = json.Unmarshal(config, &objmap)
+		if err != nil {
+			log.Fatal("Failed to process config file : ", err.Error())
+		}
+
+		if Config == nil {
+			Config = make(map[string]NullValue)
+		}
+
+		for key, value := range objmap {
+			val := string(*value)
+
+			if len(val) > 0 && val[0] == '"' {
+				val = val[1:]
+			}
+			if len(val) > 0 && val[len(val)-1] == '"' {
+				val = val[:len(val)-1]
+			}
+
+			Config[key] = NullValue{Value: val, NotNull: true}
+			if print {
+				log.Println(key, "=", val)
+			}
+		}
+
+		ConfigFileName = NullValue{Value: file.Name(), NotNull: true}
+
+		ConfigFileParsed = true
+	} else {
+		log.Println("\nConfig tag not set\n\t '" + os.Args[0] + "' --config /path/to/json/configfile'")
+	}
 
 	// log.Println("Config loaded")
 
@@ -81,17 +92,21 @@ func NewConfig() {
 }
 
 /*
-	Get a variable from JSON config instance
-	variable - variable name
-	return value
+Get a variable from JSON config instance
+variable - variable name
+return value
 */
 func Get(variable string) string {
+	if !ConfigFileParsed {
+		return os.Getenv(variable)
+	}
+
 	if !Config[variable].NotNull {
 		env := os.Getenv(variable)
 
 		if env != "" {
-            return env
-        }
+			return env
+		}
 
 		log.Fatal("'" + variable + "' not set as an enviromental variable or in config file: '" + ConfigFileName.Value + "'")
 	}
